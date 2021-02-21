aa3d6d4d73fb25fe7d0634392080947bb21520eb
jessicarolyne <jessicarolyne@hotmail.com>
2021-02-21 15:52:41 -0300

    Página inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $categorias = [];
+    $categorias[] = 'Infantil';
+    $categorias[] = 'Adolescente';
+    $categorias[] = 'Adulto';
+    //print_r($categorias)
+
+    $nome = "Jessica";
+    $idade = 24;
+
+    //var_dump($nome);
+    //var_dump($idade);
+
+    if($idade >= 6 && $idade < 12){
+        for($i = 0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'Infantil')
+                echo("O nadador $nome compete na categoria $categorias[$i]");
+        }
+    }else if($idade >=12 && $idade < 18){
+        for($i = 0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'Adolescente')
+                echo("O nadador $nome compete na categoria $categorias[$i]");
+        }
+    } else if($idade >= 18){
+        for($i = 0; $i <= count($categorias); $i++){
+            if($categorias[$i] == 'Adulto')
+                echo("O nadador $nome compete na categoria $categorias[$i]");
+        }
+    }
+?>
